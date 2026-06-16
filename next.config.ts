c9422b5714e294f34fe1f51f852806f586e4d246
Ivan Baez <155043756+Ivan-Baez@users.noreply.github.com>
2026-06-16 17:38:02 -0300

    enable unoptimized images for static export

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
-// next.config.ts
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
   allowedDevOrigins: ['192.168.1.53'],
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
